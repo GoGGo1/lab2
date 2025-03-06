47ed06e4ec467d09288ce33542d00a78bca3f920
Claudiu <alexandruclaudiu.dincu@ulbsibiu.ro>
2025-03-06 10:55:06 +0200

    Primul Comit

--- src/Application.java
+++ src/Application.java
@@ -12,6 +12,7 @@ public class Application {
         System.out.println("Triangle2 equals to Triangle1: " + tri.equals(tri2));
         Triangle tri3 = new Triangle(1.1F, 2.0F, "brown");
         System.out.println("Triangle3 equals to Triangle1: " + tri.equals(tri3));
+
     }
 }
 
